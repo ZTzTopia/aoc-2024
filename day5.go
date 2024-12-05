1607c5a66899a29f57c29e0847cfdd7269cde30e
ztz <36158219+ZTzTopia@users.noreply.github.com>
2024-12-05 22:05:00 +0800

    add day 5

---
+++ day5.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func processUpdates(input string, checkSorted bool) string {
+	parts := strings.Split(input, "\n\n")
+
+	rulesSections := strings.Split(parts[0], "\n")
+	updateSections := strings.Split(parts[1], "\n")
+
+	rules := make(map[string]bool)
+	for _, rule := range rulesSections {
+		rules[rule] = true
+	}
+
+	sum := 0
+	for _, updateSection := range updateSections {
+		updateParts := strings.Split(updateSection, ",")
+		sort.Slice(updateParts, func(i, j int) bool {
+			l := updateParts[i]
+			r := updateParts[j]
+
+			if _, exists := rules[l+"|"+r]; exists {
+				return true
+			}
+
+			if _, exists := rules[r+"|"+l]; exists {
+				return false
+			}
+
+			return false
+		})
+
+		sortedUpdateSection := strings.Join(updateParts, ",")
+		if checkSorted && updateSection != sortedUpdateSection {
+			continue
+		}
+
+		if !checkSorted && updateSection == sortedUpdateSection {
+			continue
+		}
+
+		middleIndex := len(updateParts) / 2
+		middleValue, err := strconv.Atoi(updateParts[middleIndex])
+		if err == nil {
+			sum += middleValue
+		}
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func (*PuzzleSolver) Day5Part1(input string) string {
+	return processUpdates(input, true)
+}
+
+func (*PuzzleSolver) Day5Part2(input string) string {
+	return processUpdates(input, false)
+}
